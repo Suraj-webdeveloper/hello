0dff61cee8a927fc4bb7e1a11daa9a377c9ec3a1
Suraj-webdeveloper <surajsharma4313@gmail.com>
2024-08-12 10:44:19 +0530

    first commit

--- home/urls.py
+++ home/urls.py
@@ -4,7 +4,7 @@ from home import views
 
 urlpatterns = [
    path("", views.index, name="home"),
-   path("about.html", views.about, name="about"),
+   path("about.html/", views.about, name="about"),
    path("services", views.services, name="services"),
    path("contact", views.contact, name="contact"),
 
